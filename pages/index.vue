ed0084f1617505fc97ea172883277d6bc53f4cb2
bh39dyby <bh39dyby@uni-leipzig.de>
2022-08-24 18:35:33 +0200

    formkit Test

---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="text-blue-500 text-xl">Tailwind classes test</h1>
+    <FormKit type="text" label="FormKit test" help="the config.classes definitions apply globally"/>
+</template>
